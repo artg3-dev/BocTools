a2820c6ce224b383fbd8a286bbb85d2b931d0937
artg.dev <artg3.dev@gmail.com>
2020-01-11 16:10:35 -0500

    Add gabion wall class
    GSF will be composed of gabion walls and a sandwich filter(to be made)

---
+++ src/structures/GabionWall.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package structures;
+
+import Materials.Gabion;
+import Materials.Material;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.HashMap;
+import measurements.CustomaryUnit;
+import measurements.Measurement;
+import measurements.Quantity;
+
+/**
+ *
+ * @author A3
+ */
+public class GabionWall extends Structure {
+    private EnumMap<Gabion, Integer> basketCount;
+    private static final Measurement width = 
+            new Measurement(3, CustomaryUnit.FOOT);
+    
+    public GabionWall(Measurement length, Measurement height) {
+        super("Gabion Wall", 
+                new ArrayList<Material>(Arrays.asList(Material.GABION_STONE)), 
+                        length,
+                        width,
+                        height
+                        );
+        this.basketCount = Gabion.getBasketCount(length, height);
+    }
+    
+    @Override
+    public Quantity getQuantity(Material m) {
+    }
+    
+    @Override
+    public HashMap<Material, Quantity> getQuantities() {
+    }
+    
+}
