6ae8af3ced9b1914e5b99351a5a5dfce4e8e72f7
artg.dev <artg3.dev@gmail.com>
2020-01-11 15:48:00 -0500

    Added static measurement types
    Added int values for each type of measurement, removed the static ints from the class gabion to make it more globally coherent.

--- src/measurements/Measurement.java
+++ src/measurements/Measurement.java
@@ -10,6 +10,10 @@ package measurements;
  * @author A3
  */
 public class Measurement implements Convertable {
+    public static int LENGTH = 0;
+    public static int WIDTH = 1;
+    public static int HEIGHT = 2;
+    
 
     private double value;
     private CustomaryUnit unit;
