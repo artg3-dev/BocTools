25a0e564fcfcc07837108aefee062ef506e9aa2f
artg.dev <artg3.dev@gmail.com>
2020-01-12 11:54:46 -0500

    changed abstract methods
    made abstract methods defined in GSF class because the applied equally to each gsf type

--- src/structures/GabionSandwichFilter.java
+++ src/structures/GabionSandwichFilter.java
@@ -21,18 +21,11 @@ import measurements.Quantity;
  */
 public abstract class GabionSandwichFilter extends Structure {
 
-    public static int FILTER = 0;
-    public static int OUTTER_WALL_A = 1;
-    public static int OUTTER_WALL_B = 2;
-    public static int OUTTER_WALL_C = 3;
-    public static int INNER_WALL_A = 4;
-    public static int INNER_WALL_B = 5;
-    public static int INNER_WALL_C = 6;
+    public static final int FILTER = 0;
 
-    private EnumMap<Gabion, Integer> baskets;
-    private static final Measurement width = new Measurement(3,
-            CustomaryUnit.FOOT);
-    private HashMap<Integer, Structure> components;
+    protected EnumMap<Gabion, Integer> baskets;
+
+    protected HashMap<Integer, Structure> components;
 
     public GabionSandwichFilter(Material sandType) {
         super("Gabion sandwich filter", new ArrayList<Material>(Arrays.asList(
@@ -51,30 +44,73 @@ public abstract class GabionSandwichFilter extends Structure {
 
         return null;
     }
+    
+    public EnumMap<Gabion, Integer> getTotalBasketCount() {
+        EnumMap<Gabion, Integer> count = new EnumMap(Gabion.class);
+        count.put(Gabion.BASKET_9X3X3, 0);
+        count.put(Gabion.BASKET_6X3X3, 0);
 
+        //parses over each component
+        for (int i : components.keySet()) {
+            //only handles gabionwall components
+            if (components.get(i).getClass() == GabionWall.class) {
+                GabionWall w = (GabionWall) components.get(i);
+                //adds each count to the total count "count"
+                for (Gabion j : w.getBasketCount().keySet()) {
+                    count.put(j, w.getBasketCount().get(j) + count.get(j));
+                }
+            }
+        }
+
+        return count;
+    }
+    
+    public EnumMap<Gabion, Integer> getBasketCount(int wallType) {
+        if (components.containsKey(wallType)) {
+            GabionWall w = (GabionWall) components.get(wallType);
+            return w.getBasketCount();
+        }
+
+        return null;
+    }
+    
     @Override
     public HashMap<Material, Quantity> getQuantities() {
         HashMap<Material, Quantity> qntys = new HashMap<>();
 
         // Parses over each component in the structure by int i (type)
         for (Integer i : components.keySet()) {
-            HashMap<Material, Quantity> componentQntys = 
-                    components.get(i).getQuantities();
+            HashMap<Material, Quantity> componentQntys
+                    = components.get(i).getQuantities();
 
             //adds each material to the master qntys.
             for (Material j : componentQntys.keySet()) {
                 if (!qntys.containsKey(j)) {
                     qntys.put(j, componentQntys.get(j));
-                } else{
+                } else {
                     qntys.put(j, qntys.get(j).add(componentQntys.get(j)));
                 }
             }
         }
-        
+
         return qntys;
     }
 
-    abstract public EnumMap<Gabion, Integer> getTotalBasketCount();
+    @Override
+    public Quantity getQuantity(Material m) {
+        Quantity q = null;
 
-    abstract public EnumMap<Gabion, Integer> getBasketCount(GabionWall g);
+        //parses over each component
+        for (int i : components.keySet()) {
+            //if the component contains the material in question
+            if (components.get(i).materials.contains(m)) {
+                if (q == null) {
+                    q = components.get(i).getQuantity(m);
+                } else {
+                    q = q.add(components.get(i).getQuantity(m));
+                }
+            }
+        }
+        return q;
+    }
 }
