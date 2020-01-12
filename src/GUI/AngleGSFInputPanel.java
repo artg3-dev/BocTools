800bacdaa05ef8d2ea661e4ca5f862312b2ca077
artg.dev <artg3.dev@gmail.com>
2020-01-12 14:05:22 -0500

    add angle gsfinputpanel

---
+++ src/GUI/AngleGSFInputPanel.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import javax.swing.BoxLayout;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author A3
+ */
+public class AngleGSFInputPanel extends JPanel {
+
+    private JCheckBox isSimple;
+    private JLabel sideALabel, sideBLabel;
+    private JTextField sideAInput, sideBInput;
+
+    public AngleGSFInputPanel() {
+        super();
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        
+        isSimple = new JCheckBox("Simple");
+        isSimple.setSelected(true);
+        add(isSimple);
+        
+        sideALabel = new JLabel("Side A length (ft)");
+        add(sideALabel);
+        
+        sideAInput = new JTextField();
+        sideAInput.setColumns(6);
+        add(sideAInput);
+        
+        sideBLabel = new JLabel("Side B length (ft)");
+        add(sideBLabel);
+        
+        sideBInput = new JTextField();
+        sideBInput.setColumns(6);
+        add(sideAInput);
+    }
+
+}
