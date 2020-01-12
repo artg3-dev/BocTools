27699c20a8b4fc92d13bfbff2c044102eac60d93
artg.dev <artg3.dev@gmail.com>
2020-01-12 13:51:31 -0500

    add input panel for basic gsf

---
+++ src/GUI/BasicGSFInputPanel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author A3
+ */
+public class BasicGSFInputPanel extends JPanel {
+
+    public BasicGSFInputPanel() {
+        super();
+        
+    }
+
+}
