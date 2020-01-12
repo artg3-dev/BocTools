15a3a9d68b138710656978108daeeff793ec764d
artg.dev <artg3.dev@gmail.com>
2020-01-12 11:01:07 -0500

    added add and subtract methods to quantities

--- src/measurements/Quantity.java
+++ src/measurements/Quantity.java
@@ -12,24 +12,45 @@ import Materials.Material;
  * @author A3
  */
 public class Quantity {
+
     private final Material mat;
     private final Measurement measurement;
-    
+
     public Quantity(Measurement m, Material mat) {
         this.measurement = m;
         this.mat = mat;
     }
-    
+
     public Material getMaterial() {
         return this.mat;
+    }
+
+    public Quantity add(Quantity q) throws IllegalArgumentException {
+        if (this.mat == q.mat) {
+            Measurement compatable
+                    = this.measurement.getCompatable(q.measurement);
+            return new Quantity(this.measurement.add(compatable), this.mat);
+        }
+
+        throw new IllegalArgumentException(
+                "Cannot add quantities of different materials");
+    }
+
+    public Quantity subtract(Quantity q) throws IllegalArgumentException {
+        if (this.mat == q.mat) {
+            Measurement compatable
+                    = this.measurement.getCompatable(q.measurement);
+            return new Quantity(this.measurement.subtract(compatable), 
+                    this.mat);
+        }
+
+        throw new IllegalArgumentException(
+                "Cannot subtract quantities of different materials");
     }
 
     @Override
     public String toString() {
         return measurement + " of " + mat.toString();
     }
-    
-    
-    
-    
+
 }
