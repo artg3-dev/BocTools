aea80f7102810437913d0d5b35d2cdf928bb74fa
artg.dev <artg3.dev@gmail.com>
2020-01-12 11:11:19 -0500

    rework gsf class
    made the class abstract, and added support for future child classes that expand on it.

--- src/structures/GabionSandwichFilter.java
+++ src/structures/GabionSandwichFilter.java
@@ -6,10 +6,10 @@
 package structures;
 
 import Materials.Gabion;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import Materials.Material;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.EnumMap;
 import measurements.CustomaryUnit;
 import measurements.Measurement;
@@ -19,14 +19,62 @@ import measurements.Quantity;
  *
  * @author A3
  */
-public abstract class GabionSandwichFilter  {
-    private EnumMap<Gabion, Integer> baskets;
-    private static final Measurement width = new Measurement(3, 
-            CustomaryUnit.FOOT);
-    private ArrayList<Structure> components;
+public abstract class GabionSandwichFilter extends Structure {
 
-    public GabionSandwichFilter(ArrayList<Structure> components) {
+    public static int FILTER = 0;
+    public static int OUTTER_WALL_A = 1;
+    public static int OUTTER_WALL_B = 2;
+    public static int OUTTER_WALL_C = 3;
+    public static int INNER_WALL_A = 4;
+    public static int INNER_WALL_B = 5;
+    public static int INNER_WALL_C = 6;
+
+    private EnumMap<Gabion, Integer> baskets;
+    private static final Measurement width = new Measurement(3,
+            CustomaryUnit.FOOT);
+    private HashMap<Integer, Structure> components;
+
+    public GabionSandwichFilter(Material sandType) {
+        super("Gabion sandwich filter", new ArrayList<Material>(Arrays.asList(
+                Material.GABION_STONE,
+                sandType,
+                Material.NO1_STONE,
+                Material.WOVEN_GEO)));
         this.baskets = new EnumMap<>(Gabion.class);
-        this.components = components;
+        this.components = new HashMap();
     }
+
+    public Structure getComponent(int type) {
+        if (components.containsKey(type)) {
+            return components.get(type);
+        }
+
+        return null;
+    }
+
+    @Override
+    public HashMap<Material, Quantity> getQuantities() {
+        HashMap<Material, Quantity> qntys = new HashMap<>();
+
+        // Parses over each component in the structure by int i (type)
+        for (Integer i : components.keySet()) {
+            HashMap<Material, Quantity> componentQntys = 
+                    components.get(i).getQuantities();
+
+            //adds each material to the master qntys.
+            for (Material j : componentQntys.keySet()) {
+                if (!qntys.containsKey(j)) {
+                    qntys.put(j, componentQntys.get(j));
+                } else{
+                    qntys.put(j, qntys.get(j).add(componentQntys.get(j)));
+                }
+            }
+        }
+        
+        return qntys;
+    }
+
+    abstract public EnumMap<Gabion, Integer> getTotalBasketCount();
+
+    abstract public EnumMap<Gabion, Integer> getBasketCount(GabionWall g);
 }
