a905e58ef2ffe4f3f33bbecba34458f5f99714d5
artg.dev <artg3.dev@gmail.com>
2020-01-13 20:36:24 -0500

    changed basicgsf layout to gridbag

--- src/GUI/BasicGSFInputPanel.java
+++ src/GUI/BasicGSFInputPanel.java
@@ -7,11 +7,11 @@ package GUI;
 
 import GUI.comboboxes.FilterComboBox;
 import Materials.Material;
-import javax.swing.BoxLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import measurements.CustomaryUnit;
 import measurements.Measurement;
 import structures.BasicGSF;
 import structures.GabionSandwichFilter;
@@ -28,16 +28,29 @@ public class BasicGSFInputPanel extends JPanel implements HasGSF {
 
     public BasicGSFInputPanel() {
         super();
-        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        
         label = new JLabel("Length (ft)");
         inputLength = new JTextField();
         inputLength.setColumns(6);
         filterType = new FilterComboBox();
-
-        add(label);
-        add(inputLength);
-        add(filterType);
+        
+        //label
+        c.gridx = 0;
+        c.gridy = 1;
+        add(label, c);
+        
+        //input
+        c.gridx = 1;
+        c.gridy = 1;
+        add(inputLength, c);
+        
+        //combobox
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 2;
+        add(filterType, c);
     }
 
     @Override
