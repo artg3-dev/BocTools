e92745301db65d9693036d304b5ac655542c1e96
artg.dev <artg3.dev@gmail.com>
2020-01-13 21:12:58 -0500

    added boctoolsgui
    the runnable part of the boctools gui, contains the frame for the mainpanel

--- src/GUI/BOCToolsGUI.java
+++ src/GUI/BOCToolsGUI.java
@@ -5,10 +5,28 @@
  */
 package GUI;
 
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 /**
  *
  * @author A3
  */
-public class BOCToolsGUI {
+public class BOCToolsGUI implements Runnable{
+    private JFrame frame;
+    public BOCToolsGUI() {
+    }
+    
+    @Override
+    public void run() {
+        frame = new JFrame("BOC Estimation Tool");
+        frame.setPreferredSize(new Dimension(600, 300));
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.pack();
+        frame.setVisible(true);
+    }
     
 }
