65b06926c5147fa7c6636e7460366a047e6e33d2
artg.dev <artg3.dev@gmail.com>
2020-01-13 19:41:42 -0500

    implemented HasGSF in Basic input panel

--- src/GUI/BasicGSFInputPanel.java
+++ src/GUI/BasicGSFInputPanel.java
@@ -6,20 +6,26 @@
 package GUI;
 
 import GUI.comboboxes.FilterComboBox;
+import Materials.Material;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import measurements.CustomaryUnit;
+import measurements.Measurement;
+import structures.BasicGSF;
+import structures.GabionSandwichFilter;
 
 /**
  *
  * @author A3
  */
-public class BasicGSFInputPanel extends JPanel {
+public class BasicGSFInputPanel extends JPanel implements HasGSF {
+
     private JLabel label;
     private JTextField inputLength;
     private FilterComboBox filterType;
-    
+
     public BasicGSFInputPanel() {
         super();
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
@@ -28,9 +34,24 @@ public class BasicGSFInputPanel extends JPanel {
         inputLength = new JTextField();
         inputLength.setColumns(6);
         filterType = new FilterComboBox();
-        
+
         add(label);
         add(inputLength);
         add(filterType);
+    }
+
+    @Override
+    public GabionSandwichFilter getGSF() throws IllegalArgumentException {
+        if (inputLength.getText().equals("")) {
+            throw new IllegalArgumentException("You must input a length");
+        }
+        try {
+            Material sandType = filterType.getMaterial();
+            Measurement length = new Measurement(Double.parseDouble(
+                    inputLength.getText()), CustomaryUnit.FOOT);
+            return new BasicGSF(sandType, length, new Measurement(3));
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Length must be a number");
+        }
     }
 }
